b4d7f15c4017285664016bd60c7541befe33d116
CheNikos <chenikos@yandex.ru>
2023-03-20 19:57:55 +0300

    Реализовал удаление только своей карточки

--- controllers/cards.js
+++ controllers/cards.js
@@ -29,15 +29,17 @@ const deleteCard = (req, res) => {
   const { cardId } = req.params;
 
   cardSchema
-    .findByIdAndRemove(cardId)
+    .findById(cardId)
     .then((card) => {
-      if (card) return res.send(card);
-
-      return res
-        .status(404)
-        .send({ message: 'Карточка с указанным id не найдена' });
+      if (!card) {
+        return res.status(401).send({ message: 'Карточка с указанным id не найдена' });
+      }
+      if (!(card.owner.equals(req.user._id.toString()))) {
+        return res.status(400).send({ message: 'Чужая карточка не может быть удалена' });
+      }
+      return card.findByIdAndRemove().then(() => res.send({ message: 'Карточка удалена' }));
     })
-    .catch(() => res.status(400).send({ message: 'Переданы некорректные данные при создании карточки' }));
+    .catch(() => res.status(404).send({ message: 'Переданы некорректные данные карточки' }));
 };
 
 const likeCard = (req, res) => {
